0437460f8f631f1364f26b48d05865e7d3644174
Zavier Cassidy <zaviercassidy07@gmail.com>
2026-04-03 20:38:16 +1100

    Kernel now 4096b, it also now supports input, but does nothing with it, only print

--- compile.sh
+++ compile.sh
@@ -7,4 +7,4 @@ nasm -f bin kernel/kernel.asm -o build/kernel.bin
 dd if=/dev/zero of=out/os.img bs=512 count=2880
 dd if=build/stage1.bin of=out/os.img bs=512 count=1 conv=notrunc
 dd if=build/stage2.bin of=out/os.img bs=512 count=4 seek=1 conv=notrunc
-dd if=build/kernel.bin of=out/os.img bs=512 count=1 seek=5 conv=notrunc
+dd if=build/kernel.bin of=out/os.img bs=512 count=8 seek=5 conv=notrunc
--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -12,49 +12,73 @@ xor sp, sp
 mov ax, 0x900
 mov ds, ax ; address to read = ds * 16 + offset, so this sets ds to 0x9000, where we are
 
-mov si, msg ; put msg in source
+start: ; this label does nothing, just here to clearly show where program starts
+    call main
+    jmp $ ; loop after main completes
 
-.printChar:
+main:
+    mov si, msg
+    call print
+    call newLine
+    mov si, msg2
+    call print
+    call newLine
+    call newLine
+
+    jmp .loop
+.loop:
+    call getKey
+    jmp .loop
+
+print:
     lodsb ; mov al, [si] then inc si
     cmp al, 0 ; if terminate
-    je .stackTest
-    mov ah, 0x0E ; write arguement
-    int 0x10 ; bios call
-    jmp .printChar
-
-.stackTest:
-    mov ax, 't'
-    push ax
-    mov ax, 's'
-    push ax
-    mov ax, 'e'
-    push ax
-    mov ax, 't'
-    push ax
-    mov ax, 10
-    push ax
-
-    pop ax
-    mov ah, 0x0E
-    int 0x10
-    pop ax
-    mov ah, 0x0E
-    int 0x10
-    pop ax
-    mov ah, 0x0E
-    int 0x10
-    pop ax
-    mov ah, 0x0E
-    int 0x10
-    pop ax
-    mov ah, 0x0E
-    int 0x10
-
-    jmp .done
-
+    je .done
+    call printChar
+    jmp print
 .done:
-    jmp $ ; loop
+    ret
 
-msg db "Hello, World!", 0
+printChar:
+    mov ah, 0x0E
+    int 0x10
+    ret
 
-times 512 - ($ - $$) db 0 ; pad code
+newLine:
+    mov ah, 0x0E
+    mov al, 0x0D
+    int 0x10 ; print carriage return, moves to start of line
+
+    mov ah, 0x0E
+    mov al, 0x0A
+    int 0x10 ; prints newline
+
+    ret
+
+getKey:
+    xor ah, ah
+    int 0x16
+
+    cmp al, 0x0D
+    je .enter
+    cmp al, 0x08
+    je .backspace
+    jne .else
+.enter:
+    call newLine
+    ret
+.backspace:
+    call printChar
+    mov al, ' '
+    call printChar
+    mov al, 0x08
+    call printChar
+    ret
+.else:
+    call printChar
+    ret
+
+msg db "Kernel Loaded", 0
+msg2 db "Hello, World!", 0
+
+times 4096 - ($ - $$) db 0 ; pad code
