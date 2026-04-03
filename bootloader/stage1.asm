a3079cd24b796c34c70056c0f240eeefb35f6161
Zavier Cassidy <zaviercassidy07@gmail.com>
2026-04-03 17:37:15 +1100

    Two stage bootloader is now working

---
+++ bootloader/stage1.asm
@@ -0,0 +1,20 @@
+[org 0x7C00] ; where bios starts looking
+[bits 16]   ; cpu only works in 16 bit at this point, even if it supports higher later
+
+xor ax, ax ; zero register 
+mov ds, ax
+mov es, ax ; zero the two
+mov bx, 0x8000 ; bx contains destination address to load into
+
+mov ah, 0x02 ; code for read operation
+mov al, 1 ; number of sectors to read
+mov ch, 0 ; cylinder number to read
+mov cl, 2 ; sector number, 0 is index, 1 is this one, 2 is next
+mov dh, 0 ; head number to read
+int 0x13 ; bios call for disk operation
+
+jmp 0x8000 ; jump to where it was loaded
+; we need to load a second file as this one has a limit of 512B. I don't think we're close to that but its just easier to get rid of that limit
+
+times 510 - ($ - $$) db 0 ; writes all but last 2 bytes 0, program has to be 512 long
+dw 0xAA55 ; boot loader code, if this isnt in last two bytes it will refuse to boot
--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 # !/bin/bash
-nasm -f bin bootloader/boot.asm -o build/boot.bin
+nasm -f bin bootloader/stage1.asm -o build/stage1.bin
+nasm -f bin bootloader/stage2.asm -o build/stage2.bin
+
 dd if=/dev/zero of=out/os.img bs=512 count=2880
-dd if=build/boot.bin of=out/os.img conv=notrunc
+dd if=build/stage1.bin of=out/os.img bs=512 count=1 conv=notrunc
+dd if=build/stage2.bin of=out/os.img bs=512 count=1 seek=1 conv=notrunc
