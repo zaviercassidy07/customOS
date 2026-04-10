a85fe6aa5ab6b3b7b84a12347694930b003eaaad
Zavier Cassidy <zaviercassidy07@gmail.com>
2026-04-10 16:26:27 +1000

    Gutted kernel asm to get bare minimum working for 64 bit

--- kernel/kernel_asm.asm
+++ kernel/kernel_asm.asm
@@ -26,80 +26,9 @@ protected_mode_entry:
 
     call initPaging
 
-    call initPIC
-    call initIDT
-
-    ; define C functions
-    extern main
-    extern keyboardHandler_c
-
-    call main
+    mov word [0xB8000], 0x0F41
 
     jmp $
-
-keyboardHandler_asm:
-    pushad
-
-    in al, 0x60 ; put scancode of keypress in al
-
-    test al, 0x80 ; ignore if key release
-    jnz .done
-
-    movzx eax, al ; move scancode to EAX
-    push eax ; C functions look off top of stack for arguements
-    call keyboardHandler_c ; call an external function
-    add esp, 4 ; increase stack pointer by 32 bits, effectively removing the arguement from the stack
-
-    jmp .done
-.done:
-    mov al, 0x20
-    out 0x20, al ; end interupt, program continues
-
-    popad
-    iret
-
-initPIC:
-    mov al, 0x11 ; 0x11 is init sequence, tell command for each we are doing init sequence
-    out 0x20, al
-    out 0xA0, al
-
-    mov al, 0x20 ; this block sets offset, we have to set offset as default range is reserved by CPU for other stuff, so we move ints up
-    out 0x21, al
-    mov al, 0x28
-    out 0xA1, al
-
-    mov al, 0x04 ; this block sets up the slave PIC to talk to the master PIC right, slave connects to master IRQ2
-    out 0x21, al
-    mov al, 0x02
-    out 0xA1, al
-
-    mov al, 0x01 ; set PIC to x86 mode
-    out 0x21, al
-    out 0xA1, al
-
-    mov al, 0xFD ; enable only keyboard interupts
-    out 0x21, al
-    mov al, 0xFF
-    out 0xA1, al
-
-    ret
-
-initIDT:
-    ; write keyboard interrupt code
-    mov eax, keyboardHandler_asm
-
-    mov word [idt_start + 0x21*8], ax ; move lower half of keyboardHandler address to start of int code
-    shr eax, 16 ; move higher half of eax to lower half
-    mov word [idt_start + 0x21*8 + 6], ax ; move higher half of address now to end of int code
-
-    mov word [idt_start + 0x21*8 + 2], 0x08 ; fill in middle info
-    mov byte [idt_start + 0x21*8 + 4], 0
-    mov byte [idt_start + 0x21*8 + 5], 0x8E
-
-    lidt [idtr] ; load idt
-    sti ; restart interupts now that my own interupt table is loaded and we're in 32bit
-
-    ret
 
 ; current paging is 1:1 mapping of virtual to physical memory, really no benefit to paging at this point but it allows expansion later
 initPaging:
@@ -138,14 +67,6 @@ gdt_end:
 gdtr:
     dw gdt_end - gdt_start - 1 ; length of gdt - 1
     dd gdt_start ; start address
-
-idt_start:
-    times 256 dq 0 ; pad to 256 entries
-idt_end:
-
-idtr:
-    dw idt_end - idt_start - 1 ; length of idt - 1
-    dd idt_start ; same as gdt
 
 align 4096 ; moves address forward until divisible by 4096, these tables need to be on even address
 page_directory: ; contains pointer to tables
